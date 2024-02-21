2ac9d927cad83648e89008fa566b98b3b0d92ee8
unknown <priyobrotachy@gmail.com>
2024-02-21 12:07:40 +0600

    Add your commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/admin/')
+def adminIndex():
+    return render_template('admin/index.html', title="Admin Login")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
